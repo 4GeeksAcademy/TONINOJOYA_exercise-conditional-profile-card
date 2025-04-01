e909b9dbd7d6f9c82378edb857246b8c48947b10
ToninoJoya <antoniocaprab@gmail.com>
2025-04-01 01:30:43 +0000

    Solo cambios en celdas basicas

--- src/js/app.js
+++ src/js/app.js
@@ -35,8 +35,10 @@ function render(variables = {}) {
           <img src="${variables.avatarURL}" class="photo" />
           <h1>${variables.name == null ? "Name" : variables.name} 
           ${variables.lastName == null ? "Last Name" : variables.lastName}</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
+          <h2>${variables.role == null ? "Role" : variables.role}</h2>
+          <h3>${variables.city == null ? "City" : variables.city}, ${
+    variables.country == null ? "Country" : variables.country
+  } </h3>
           <ul class="position-right">
             <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
             <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
